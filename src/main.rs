e72663f8c129f706f51ce50fc6c4b90db89ea61f
Josh Stone <jistone@redhat.com>
2017-05-04 16:03:05 -0700

    Use a macro to reduce clap boilerplate a little

--- src/main.rs
+++ src/main.rs
@@ -18,121 +18,80 @@ fn main() {
     }
 }
 
-fn combine(matches: &ArgMatches) {
-    let mut com = Combiner::default();
-    matches
-        .value_of("product-name")
-        .map(|s| com.product_name(s));
-    matches
-        .value_of("package-name")
-        .map(|s| com.package_name(s));
-    matches
-        .value_of("rel-manifest-dir")
-        .map(|s| com.rel_manifest_dir(s));
-    matches
-        .value_of("success-message")
-        .map(|s| com.success_message(s));
-    matches
-        .value_of("legacy-manifest-dirs")
-        .map(|s| com.legacy_manifest_dirs(s));
-    matches
-        .value_of("input-tarballs")
-        .map(|s| com.input_tarballs(s));
-    matches
-        .value_of("non-installed-overlay")
-        .map(|s| com.non_installed_overlay(s));
-    matches
-        .value_of("work-dir")
-        .map(|s| com.work_dir(s));
-    matches
-        .value_of("output-dir")
-        .map(|s| com.output_dir(s));
+/// Parse clap arguements into the type constructor.
+macro_rules! parse(
+    ($matches:expr => $type:ty { $( $option:tt => $setter:ident, )* }) => {
+        {
+            let mut command: $type = Default::default();
+            $( $matches.value_of($option).map(|s| command.$setter(s)); )*
+            command
+        }
+    }
+);
 
-    if let Err(e) = com.run() {
+fn combine(matches: &ArgMatches) {
+    let combiner = parse!(matches => Combiner {
+        "product-name" => product_name,
+        "package-name" => package_name,
+        "rel-manifest-dir" => rel_manifest_dir,
+        "success-message" => success_message,
+        "legacy-manifest-dirs" => legacy_manifest_dirs,
+        "input-tarballs" => input_tarballs,
+        "non-installed-overlay" => non_installed_overlay,
+        "work-dir" => work_dir,
+        "output-dir" => output_dir,
+    });
+
+    if let Err(e) = combiner.run() {
         println!("failed to combine installers: {}", e);
         std::process::exit(1);
     }
 }
 
 fn generate(matches: &ArgMatches) {
-    let mut gen = Generator::default();
-    matches
-        .value_of("product-name")
-        .map(|s| gen.product_name(s));
-    matches
-        .value_of("component-name")
-        .map(|s| gen.component_name(s));
-    matches
-        .value_of("package-name")
-        .map(|s| gen.package_name(s));
-    matches
-        .value_of("rel-manifest-dir")
-        .map(|s| gen.rel_manifest_dir(s));
-    matches
-        .value_of("success-message")
-        .map(|s| gen.success_message(s));
-    matches
-        .value_of("legacy-manifest-dirs")
-        .map(|s| gen.legacy_manifest_dirs(s));
-    matches
-        .value_of("non-installed-overlay")
-        .map(|s| gen.non_installed_overlay(s));
-    matches
-        .value_of("bulk-dirs")
-        .map(|s| gen.bulk_dirs(s));
-    matches
-        .value_of("image-dir")
-        .map(|s| gen.image_dir(s));
-    matches
-        .value_of("work-dir")
-        .map(|s| gen.work_dir(s));
-    matches
-        .value_of("output-dir")
-        .map(|s| gen.output_dir(s));
+    let generator = parse!(matches => Generator {
+        "product-name" => product_name,
+        "component-name" => component_name,
+        "package-name" => package_name,
+        "rel-manifest-dir" => rel_manifest_dir,
+        "success-message" => success_message,
+        "legacy-manifest-dirs" => legacy_manifest_dirs,
+        "non-installed-overlay" => non_installed_overlay,
+        "bulk-dirs" => bulk_dirs,
+        "image-dir" => image_dir,
+        "work-dir" => work_dir,
+        "output-dir" => output_dir,
+    });
 
-    if let Err(e) = gen.run() {
+    if let Err(e) = generator.run() {
         println!("failed to generate installer: {}", e);
         std::process::exit(1);
     }
 }
 
 fn script(matches: &ArgMatches) {
-    let mut scr = Scripter::default();
-    matches
-        .value_of("product-name")
-        .map(|s| scr.product_name(s));
-    matches
-        .value_of("rel-manifest-dir")
-        .map(|s| scr.rel_manifest_dir(s));
-    matches
-        .value_of("success-message")
-        .map(|s| scr.success_message(s));
-    matches
-        .value_of("legacy-manifest-dirs")
-        .map(|s| scr.legacy_manifest_dirs(s));
-    matches
-        .value_of("output-script")
-        .map(|s| scr.output_script(s));
+    let scripter = parse!(matches => Scripter {
+        "product-name" => product_name,
+        "rel-manifest-dir" => rel_manifest_dir,
+        "success-message" => success_message,
+        "legacy-manifest-dirs" => legacy_manifest_dirs,
+        "output-script" => output_script,
+    });
 
-    if let Err(e) = scr.run() {
+    if let Err(e) = scripter.run() {
         println!("failed to generate installation script: {}", e);
         std::process::exit(1);
     }
 }
 
 fn tarball(matches: &ArgMatches) {
-    let mut tar = Tarballer::default();
-    matches
-        .value_of("input")
-        .map(|s| tar.input(s));
-    matches
-        .value_of("output")
-        .map(|s| tar.output(s));
-    matches
-        .value_of("work-dir")
-        .map(|s| tar.work_dir(s));
+    let tarballer = parse!(matches => Tarballer {
+        "input" => input,
+        "output" => output,
+        "work-dir" => work_dir,
+    });
 
-    if let Err(e) = tar.run() {
+    if let Err(e) = tarballer.run() {
         println!("failed to generate tarballs: {}", e);
         std::process::exit(1);
     }
